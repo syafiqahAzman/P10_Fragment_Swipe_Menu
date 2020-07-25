2d5c43a0e9e81a134ed2557f2ccee12200be2c84
syafiqahAzman <16046491@myrp.edu.sg>
2020-07-25 16:53:19 +0800

    options menu

--- app/src/main/java/com/myapplicationdev/android/p10_fragment_swipe_menu/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p10_fragment_swipe_menu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.myapplicationdev.android.p10_fragment_swipe_menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,9 +8,11 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +37,33 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new MyFragmentPagerAdapter(fm, al);
         vPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.previous:
+                if (vPager.getCurrentItem() >0){
+                    int prevPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(prevPage, true);
+                }
+                return true;
+            case R.id.random:
+                Random randoms = new Random();
+                int ranPage = randoms.nextInt(al.size());
+                vPager.setCurrentItem(ranPage, true);
+                return true;
+            case R.id.next:
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max - 1){
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
